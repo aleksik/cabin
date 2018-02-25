e0c484cd2498ba66c67f54551c356344861b36e8
Aleksi Kaistinen <aleksi.kaistinen@gmail.com>
2018-02-25 20:05:16 +0200

    Add Bulma css framework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,11 @@ const logo = require('./logo.svg');
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App container">
+        <div className="notification">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
